0b6f3afb51874f27d51eedc97bce5f10c760c9f8
Malek Itani <malikitani.bey@gmail.com>
2026-08-21 14:41:12 +0300

    Prepare backend for Vercel deployment

--- server.js
+++ server.js
@@ -28,6 +28,10 @@ app.use("/images", express.static("images"))
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
